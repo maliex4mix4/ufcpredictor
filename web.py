d18e66211602f7c3dfdcc09fbd9852aa17495cc5
maliex4mix4 <malikopeyemi5@gmail.com>
2021-11-22 06:12:46 +0100

    done

---
+++ web.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+page_bg_img = '''
+<style>
+body {
+background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
+background-size: cover;
+}
+</style>
+'''
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
