17e2944bd5c70f556252383326f64f2048efe794
maliex4mix4 <malikopeyemi5@gmail.com>
2021-12-11 04:42:20 +0100

    Done

--- web.py
+++ web.py
@@ -6,7 +6,7 @@ import pickle
 from PIL import Image
 import plotly.graph_objects as go
 import plotly.express as px
-from sklearn.neural_network import MLPClassifier
+
 
 
 class StreamlitApp:
@@ -14,6 +14,7 @@ class StreamlitApp:
 	def __init__(self):
 		self.fighters = pd.read_csv('Data/UFC_Fighters_Database.csv')
 		self.fighters2 = pd.read_csv('Data/ufc_fighters.csv')
+		self.df2 = pd.read_csv('Data/prediction_data.csv', index_col="Unnamed: 0")
 
 	def load_models(self):
 		mlp_file = open('Models/predict_winner.pkl', 'rb')
@@ -25,38 +26,8 @@ class StreamlitApp:
 	def predict(self, df):
 		model = self.load_models()
 		prediction = model.predict_proba(df.reshape(1, -1))
-		return prediction
-
-	def construct_sidebar(self):
-
-		cols = [col for col in features.columns]
-
-		st.sidebar.markdown(
-			'<p class="header-style">Iris Data Classification</p>',
-			unsafe_allow_html=True
-		)
-		sepal_length = st.sidebar.selectbox(
-			f"Select {cols[0]}",
-			sorted(features[cols[0]].unique())
-		)
-
-		sepal_width = st.sidebar.selectbox(
-			f"Select {cols[1]}",
-			sorted(features[cols[1]].unique())
-		)
-
-		petal_length = st.sidebar.selectbox(
-			f"Select {cols[2]}",
-			sorted(features[cols[2]].unique())
-		)
-
-		petal_width = st.sidebar.selectbox(
-			f"Select {cols[3]}",
-			sorted(features[cols[3]].unique())
-		)
-		values = [sepal_length, sepal_width, petal_length, petal_width]
-
-		return values
+		pred = model.predict(df.reshape(1, -1))
+		return prediction, pred
 
 	def plot_pie_chart(self, probabilities, labels):
 		fig = go.Figure(
@@ -100,15 +71,15 @@ class StreamlitApp:
 		)
 
 		## weight class
-		fighter1_w = col1.selectbox(
+		fighter1_w = st.selectbox(
 			'Weight Class',
-			self.fighters['WeightClass'].unique()
+			self.df2['weight_class'].unique()
 		)
 
-		fighter2_w = col2.selectbox(
-			'Weight Class for Underdog',
-			self.fighters['WeightClass'].unique()
-		)
+		# fighter2_w = col2.selectbox(
+		# 	'Weight Class for Underdog',
+		# 	self.df2['weight_class'].unique()
+		# )
 
 		## Decimal Odds
 		fighter2_odd = col2.number_input(
@@ -122,14 +93,18 @@ class StreamlitApp:
 			step=0.01,
 		)
 
+		fighter_t = st.checkbox(
+			label="Is the match a Title Match?",
+		)
+
 		col1, col2, col3 = st.columns(3)
 
 		data1 = self.fighters2[self.fighters2.name == fighter1]
 		data2 = self.fighters2[self.fighters2.name == fighter2]
 
-		graph = self.plot_pie_chart([data1.win, data1.lose, data1.draw], ['Win', "Lose", "Draw"])
 
 		# col1.pyplot(graph)
+		# col3.write(self.fighters)
 
 		if col2.button('Predict'):
 			if fighter1_odd > fighter2_odd:
@@ -163,20 +138,110 @@ class StreamlitApp:
 
 				# st.dataframe(df)
 
-				pred = self.predict(np.array(df[best_cols]))
+				pred, d_pred = self.predict(np.array(df[best_cols]))
 
 				# st.write(pred)
 				col1, col2 = st.columns(2)
 
+				pred_winner = "Underdog" if d_pred[0] == 0 else 1
+
+				title_match = 0 if fighter_t == False else 1
+
+				# st.write(pred_winner)
+				weight_classes = ['weight_class_Bantamweight','weight_class_Catch Weight', 'weight_class_Featherweight','weight_class_Flyweight', 'weight_class_Heavyweight','weight_class_Light Heavyweight', 'weight_class_Lightweight','weight_class_Middleweight', 'weight_class_Open Weight','weight_class_Super Heavyweight', 'weight_class_Welterweight',"weight_class_Women's Bantamweight","weight_class_Women's Featherweight", "weight_class_Women's Flyweight","weight_class_Women's Strawweight"]
+				# fighter1_w
+				res = []
+				for x in weight_classes:
+					if fighter1_w == x.split('_')[2]:
+						res.append(1)
+					else:
+						res.append(0)
+
+				m_cols = ['winner', 'title_fight', 'SLPM_delta', 'SAPM_delta','STRD_delta', 'TDD_delta', 'SUBA_delta', 'weight_class_Bantamweight','weight_class_Catch Weight', 'weight_class_Featherweight','weight_class_Flyweight', 'weight_class_Heavyweight','weight_class_Light Heavyweight', 'weight_class_Lightweight','weight_class_Middleweight', 'weight_class_Open Weight','weight_class_Super Heavyweight', 'weight_class_Welterweight',"weight_class_Women's Bantamweight","weight_class_Women's Featherweight", "weight_class_Women's Flyweight","weight_class_Women's Strawweight"]
+				fin = [d_pred[0],
+							title_match,
+							float(data1.SLPM)-float(data2.SLPM),
+							float(data1.SAPM)-float(data2.SAPM),
+							float(data1.STRD)-float(data2.STRD),
+							float(data1.TDD)-float(data2.TDD),
+							float(data1.SUBA)-float(data2.SUBA),
+						]
+				res = np.array(res)
+				# st.write(res)
+				fin = np.array(fin)
+				dt = np.append(fin,res).reshape(1,22)
+				# st.write(dt)
+
+				map_method = {
+				    0: "Knock Out or Total Knock Out",
+				    1: "UNANIMOUS DECISION",
+				    2: "Submission",
+				    3: "Disqualification"
+				}
+
+				df2 = pd.DataFrame(
+						dt,
+						columns=m_cols
+					)
+
+				mlp_file = open('Models/predict_method.pkl', 'rb')
+				mlp_model = pickle.load(mlp_file)
+				mlp_file.close()
+
+				pred2 = mlp_model.predict(df2)
+
+				# st.write(pred2[0])
+
+				#####
+				### Predicting End round
+				#####
+				e_cols = ['winner', 'title_fight', 'method','SLPM_delta', 'SAPM_delta','STRD_delta', 'TDD_delta', 'SUBA_delta', 'weight_class_Bantamweight','weight_class_Catch Weight', 'weight_class_Featherweight','weight_class_Flyweight', 'weight_class_Heavyweight','weight_class_Light Heavyweight', 'weight_class_Lightweight','weight_class_Middleweight', 'weight_class_Open Weight','weight_class_Super Heavyweight', 'weight_class_Welterweight',"weight_class_Women's Bantamweight","weight_class_Women's Featherweight", "weight_class_Women's Flyweight","weight_class_Women's Strawweight"]
+				fin = [d_pred[0],
+							title_match,
+							pred2[0],
+							float(data1.SLPM)-float(data2.SLPM),
+							float(data1.SAPM)-float(data2.SAPM),
+							float(data1.STRD)-float(data2.STRD),
+							float(data1.TDD)-float(data2.TDD),
+							float(data1.SUBA)-float(data2.SUBA),
+						]
+				res = np.array(res)
+				# st.write(res)
+				fin = np.array(fin)
+				dt = np.append(fin,res).reshape(1,23)
+
+				df3 = pd.DataFrame(
+						dt,
+						columns=e_cols
+					)
+
+				mlp_file = open('Models/predict_end_round.pkl', 'rb')
+				mlp_model = pickle.load(mlp_file)
+				mlp_file.close()
+
+				pred3 = mlp_model.predict(df3)
+
+				# st.write(pred3[0])
+
 				col1.markdown(
-					f'<h3 class="header-style" style="text-align: center; color:red;"> Favourite: {round((pred[0][0]) * 100, 2)}% </h3>',
+					f'<h3 class="header-style" style="text-align: center; color:red;"> Favourite: {round((pred[0][1]) * 100, 2)}% </h3>',
 					unsafe_allow_html=True
 				)
 
 				col2.markdown(
-					f'<h3 class="header-style" style="text-align: center; color: blue;"> Underdog: {round((pred[0][1]) * 100, 2)}% </h3>',
+					f'<h3 class="header-style" style="text-align: center; color: blue;"> Underdog: {round((pred[0][0]) * 100, 2)}% </h3>',
 					unsafe_allow_html=True
-				)				
+				)
+
+				st.markdown(
+					f'<h3 class="header-style" style="text-align: center; color: white;"> Break Down Analysis </h3>',
+					unsafe_allow_html=True
+				)
+
+				st.markdown(
+					f'<p style="text-align: center; color: white;">The <strong>{pred_winner}</strong> will most likely win by <strong>{map_method[pred2[0]]}</strong> in round <strong>{pred3[0]}</strong>.<br/> <b>Note</b> that this result is just based on speculation and you shouldn"t bet your money based on this alone.</p>',
+					unsafe_allow_html=True
+				)
 
 		return self
 
